7932fe5f3e240411349d1b9c2d2705b12f142f29
Developer20 <senanrecebov3@gmail.com>
2016-10-11 14:12:18 -0700

    Not Finished

--- hw0/main.c
+++ hw0/main.c
@@ -2,10 +2,13 @@
 #include <sys/resource.h>
 
 int main() {
+    
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
+
+    int pL = getrlimit(RLIMIT_NPROC, &lim);
+
+    printf("\n\nstack size: %ld\n", 0L);
+    printf("process limit: %ld\n", lim.rlim_cur);
     printf("max file descriptors: %ld\n", 0L);
-    printf("Developer");
     return 0;
 }
--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,46 @@
-int main(int argc, char *argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printText();
+    numberOfLines();
+
     return 0;
+}
+
+void printText(){
+    FILE *file;
+    file =fopen("/home/developer/Text Files/file.txt", "r");
+    int count = 0;
+    char buf[1000];
+    char* str = NULL;
+
+    while ((str = fgets(buf, 1000, file)) != NULL){
+        printf("%s",str);
+        count += strlen(str);
+    }
+
+    printf("\nNumber of characthers: %d", count);
+}
+
+void numberOfLines(){
+    FILE *file;
+    file =fopen("/home/developer/Text Files/file.txt", "r");
+
+    char c;
+    int lines = 0, words = 0;
+
+    do {
+      c = getc (file);
+      if (c == '\n' && c != ' '){
+          lines++;
+        }
+      if(c == ' ' || c == '\n'){
+        words++;
+      }
+      } while (c != EOF);
+
+    printf("\nNumber of lines: %d", lines);
+    printf("\nNumber of words: %d\n", words);
 }
