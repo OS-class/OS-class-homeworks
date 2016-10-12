dd4408a96b0d772d3b18a815f07d37e3493a9f14
CoderINusE <rkerimov@std.qu.edu.az>
2016-10-12 15:11:42 -0700

    Finished my first CS162 assignment

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,28 @@
-int main(int argc, char *argv[]) {
-    return 0;
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	unsigned int words, lines, characters;
+	words = lines = characters = 0;
+	char* filename = argv[1];
+	FILE* file = fopen(filename, "r");
+	char ch = fgetc(file), prech = 's';
+	
+	if(file == NULL) { // return if there is no such file
+		printf("No such file\n");		
+		return 0;
+	}
+
+	while((ch = fgetc(file)) != EOF) {
+				
+		characters += 1;		
+		if(ch == '\n') { 
+			lines += 1;
+			words += 1; // maybe there is some optimal solution to count words
+		}
+		if(ch == ' ' && prech != ' ') {
+			words += 1;
+		}
+		prech = ch;
+	}
+	printf("%u %u %u %s\n", lines, words, characters, filename);
 }
