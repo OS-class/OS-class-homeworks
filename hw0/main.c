438976f9d291f8b222c87ee84bfb294df9e311b8
Senani Recebov <Developer20@users.noreply.github.com>
2016-10-12 16:45:14 +0400

    Update main.c

--- hw0/main.c
+++ hw0/main.c
@@ -5,10 +5,13 @@ int main() {
     
     struct rlimit lim;
 
-    int pL = getrlimit(RLIMIT_NPROC, &lim);
-
-    printf("\n\nstack size: %ld\n", 0L);
+    int stackSize = getrlimit(RLIMIT_STACK, &lim);
+    printf("\n\nstack size: %ld\n", lim.rlim_cur);
+    
+    int processLimit = getrlimit(RLIMIT_NPROC, &lim);
     printf("process limit: %ld\n", lim.rlim_cur);
-    printf("max file descriptors: %ld\n", 0L);
+    
+    int maxFileDesc = getrlimit(RLIMIT_NOFILE, &lim);
+    printf("max file descriptors: %ld\n", lim.rlim_cur);
     return 0;
 }
