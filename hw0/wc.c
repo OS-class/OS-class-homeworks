8775f711fcad64062d79f7df12a3c05948e431c3
Gunay ALqaeva <Gunay ALqaeva>
2016-10-14 00:52:27 +0500

    group02

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,42 @@
-int main(int argc, char *argv[]) {
-    return 0;
-}
++#include <stdio.h>
+ +#include <stdlib.h> 
+ +#include <ctype.h> 
+ +
+ +int main(int argc, char* argv[])
+ +{
+ +    unsigned long lCount, wCount , cCount; 
+ +
+ +    char* cFile = ""; 
+ +    cFile = argv[1]; 
+ +
+ +    FILE* fPointer = NULL; 
+ +    fPointer = fopen(cFile,"r"); 
+ +
+ +    char ch;
+ +    char previous = ' '; 
+ +
+ +    cCount =0;
+	  wCount = 0;
+	  cFile = 0;
+ +        
+ +    
+ +    while((ch = getc(fPointer)) != EOF) {
+ +        cCount++; 
+ 
+	 if(isspace(previous) && !isspace(ch))
+ +            wCount++;
+ +
+ +
+ +        if(ch == '\n')
+ +            lCount++; 
+ +
+ +       
+ +      
+ +        
+ +        previous = ch; 
+ +    }
+ +        
+ +    printf("%ld %ld %ld %s\n", lCount, wCount, cCount, cFile); 
+ +
+ +    return 0;
+ +}
